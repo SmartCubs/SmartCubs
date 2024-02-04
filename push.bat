5aaa4ae7b3e8314c15ef0f9320db2b5278f386a7
Yanix <monnah.yans@gmail.com>
2024-02-04 11:05:01 +0100

    making the scene flexibal for diff screen sizes

---
+++ push.bat
@@ -0,0 +1,19 @@
+@echo off
+
+REM Check Git status
+git status
+
+REM Add changes
+git add .
+
+REM Prompt for commit message
+set /p commitMessage=Enter commit message: 
+
+REM Commit changes with the provided message
+git commit -m "%commitMessage%"
+
+REM Push to the repository
+git push
+
+REM Pause to keep the console window open (optional)
+pause
