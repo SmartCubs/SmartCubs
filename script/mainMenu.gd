15dcd79afb8c7e0ee830b57a72a5a46ff54900bd
Yanix <monnah.yans@gmail.com>
2024-02-12 22:45:47 +0100

    adding main menu

---
+++ script/mainMenu.gd
@@ -0,0 +1,23 @@
+extends Control
+
+
+onready var animation_player = $AnimationPlayer
+onready var color_rect = $ColorRect
+
+
+func _ready():
+	pass # Replace with function body.
+
+
+
+func _on_playBTN_button_up():
+	color_rect.visible = true
+	animation_player.play("start")
+
+
+func Start_level():
+	get_tree().change_scene("res://scene\'s/levels/letter/level1.tscn")
+
+#change to parameter scene
+func _on_Button_button_up():
+	pass
