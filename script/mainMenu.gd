62df9238cefa1dbb80bfd4c2bacd650c2943ebc4
Yanix <monnah.yans@gmail.com>
2024-02-14 16:56:04 +0100

    just commiting

--- script/mainMenu.gd
+++ script/mainMenu.gd
@@ -13,15 +13,25 @@ func _ready():
 func _on_close_btn_click():
 	animation_player.play_backwards("option")
 
+#play btn event
 func _on_playBTN_button_up():
+	$playBTN.rect_scale = Vector2(1,1)
 	color_rect.visible = true
 	animation_player.play("start")
+	
 
+func _on_playBTN_button_down():
+	$playBTN.rect_scale = lerp(Vector2(1,1) ,Vector2(0.9,0.9),0.25)
 
 func Start_level():
 	get_tree().change_scene("res://scene\'s/levels/letter/level1.tscn")
 
-#change to parameter scene
+#option btn event
+	#change to parameter scene
 func _on_Button_button_up():
 	option_panel.visible = true
 	animation_player.play("option")
+	
+
+
+
