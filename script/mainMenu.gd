56fb207945826debd058826bc4c07289f5fc59d2
Yanix <monnah.yans@gmail.com>
2024-02-16 21:21:44 +0100

    changing some shit

--- script/mainMenu.gd
+++ script/mainMenu.gd
@@ -5,6 +5,7 @@ onready var animation_player = $AnimationPlayer
 onready var color_rect = $panel/playTransition
 onready var option_panel = $panel/optionMenu
 onready var btnCloseOption = option_panel.get_node("Button")
+onready var playBtn = $Button/playBTN
 
 func _ready():
 	option_panel.visible = false
@@ -17,13 +18,13 @@ func _on_close_btn_click():
 
 #play btn event
 func _on_playBTN_button_up():
-	$playBTN.rect_scale = Vector2(1,1)
+	playBtn.rect_scale = Vector2(1,1)
 	color_rect.visible = true
 	animation_player.play("start")
 	
 
 func _on_playBTN_button_down():
-	$playBTN.rect_scale = lerp(Vector2(1,1) ,Vector2(0.9,0.9),0.25)
+	playBtn.rect_scale = lerp(Vector2(1,1) ,Vector2(0.9,0.9),0.25)
 
 func Start_level():
 	get_tree().change_scene("res://scene\'s/levels/letter/level1.tscn")
