14a5c276df70803b91b7805802c2028c73ac7667
Yanix <monnah.yans@gmail.com>
2024-02-22 20:07:18 +0100

    making drawing systeme

---
+++ draw space.gd
@@ -0,0 +1,23 @@
+extends Control
+
+onready var lines := $Node2D/lines
+
+var pressed = false
+var curent_line: Line2D
+
+func _input(event: InputEvent) -> void:
+	var mouse_position = get_local_mouse_position()
+	if event is InputEventMouseButton:
+		pressed = event.pressed
+
+		# Making line
+		if pressed:
+			curent_line = Line2D.new()
+			curent_line.default_color = lines.default_color
+			curent_line.width = lines.width
+			lines.add_child(curent_line)
+
+	# Drawing lines
+	if event is InputEventMouseMotion && pressed:
+		curent_line.add_point(event.position - rect_position)
+	
