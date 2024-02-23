7c493d3da62163154771adb7edc66686953a0b4e
Yanix <monnah.yans@gmail.com>
2024-02-23 13:19:18 +0100

    adding transition root for mking transition easy

--- script/mainMenu.gd
+++ script/mainMenu.gd
@@ -2,14 +2,12 @@ extends Control
 
 
 onready var animation_player = $AnimationPlayer
-onready var color_rect = $panel/playTransition
 onready var option_panel = $panel/optionMenu
 onready var btnCloseOption = option_panel.get_node("Button")
 onready var playBtn = $Button/playBTN
 
 func _ready():
 	option_panel.visible = false
-	color_rect.visible = false
 	btnCloseOption.connect("button_up",self,"_on_close_btn_click")
 	
 
@@ -19,15 +17,11 @@ func _on_close_btn_click():
 #play btn event
 func _on_playBTN_button_up():
 	playBtn.rect_scale = Vector2(1,1)
-	color_rect.visible = true
-	animation_player.play("start")
+	SceanTransition.change_scene("res://scene\'s/levels/letter/level1.tscn")
 	
 
 func _on_playBTN_button_down():
 	playBtn.rect_scale = lerp(Vector2(1,1) ,Vector2(0.9,0.9),0.25)
-
-func Start_level():
-	get_tree().change_scene("res://scene\'s/levels/letter/level1.tscn")
 
 #option btn event
 	#change to parameter scene
