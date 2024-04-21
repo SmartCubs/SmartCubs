b1450b6c2d1117442a8c4f150899e98b40419104
Yanix <monnah.yans@gmail.com>
2024-04-21 17:47:42 +0100

    swapping bug fixed

--- scene's/menu/control_menu_UI.gd
+++ scene's/menu/control_menu_UI.gd
@@ -3,9 +3,17 @@ extends Control
 onready var btn1 = $VBoxContainer/btn1
 onready var btn2 = $VBoxContainer/btn2
 
+#########export variables
 export(String,FILE,"*.tscn") var path_btn1:String = "res://scene\'s/menu/mainMenu.tscn"
 export(String,FILE,"*.tscn") var path_btn2:String = "res://scene's/menu/level_selector_screen/level_selector.tscn"
 
+export(StreamTexture) var icon_img_btn1:StreamTexture = preload("res://asset\'s/icons/pause.png")
+export(StreamTexture) var icon_img_btn2:StreamTexture = preload("res://asset\'s/icons/house.png")
+
+
+func _ready():
+	btn1.icon = icon_img_btn1
+	btn2.icon = icon_img_btn2
 
 func _on_mainMenu_button_up():
 	SceanTransition.change_scene(path_btn1)
--- script/Btn_level.gd
+++ script/Btn_level.gd
@@ -5,4 +5,7 @@ func _ready():
 	pass
 
 func _set_img(img_name:Object):
+	yield(get_tree().create_timer(.3), "timeout")
 	icon = img_name
+
+
