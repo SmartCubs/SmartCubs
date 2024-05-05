6932ccd743b5a9807557e0d73e831e6f0b8111e7
Yanix <monnah.yans@gmail.com>
2024-05-05 19:31:52 +0100

    save sys

--- scene's/end_level/end_game.gd
+++ scene's/end_level/end_game.gd
@@ -5,7 +5,7 @@ onready var dog = $panel/character
 onready var score = $label
 
 export  var btnPath = "res://scene\'s/levels/letter/level1.tscn"
-
+const main_scean = "res://scene\'s/menu/level_selector_screen/level_selector.tscn"
 
 func _show(pose:int , text:String):
 	visible = true
@@ -16,3 +16,7 @@ func _show(pose:int , text:String):
 func _on_exit_pressed():
 	SceanTransition.change_scene(btnPath,"d")
 
+
+
+func _on_go_to_selection_menu_pressed():
+	SceanTransition.change_scene(main_scean)
