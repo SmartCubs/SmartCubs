78e3cd9bfdfe4bc6dd3a7f20dc07f895f50f8f28
Yanix <monnah.yans@gmail.com>
2024-05-06 00:16:09 +0100

    starting the drawing world :)

--- scene's/levels/drawing_world/drawing_world.gd
+++
@@ -1,5 +0,0 @@
-extends Control
-
-func _notification(what):
-	if what == NOTIFICATION_WM_GO_BACK_REQUEST:
-		SceanTransition.change_scene("res://scene\'s/menu/level_selector_screen/level_selector.tscn")
