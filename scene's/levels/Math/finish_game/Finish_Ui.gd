0c4c6b4ce92b188316d28373d5f39b1bd667e92c
Monown <syncdgold015@gmail.com>
2024-05-05 14:41:06 +0100

    fanilazing math end scene

--- scene's/levels/Math/finish_game/Finish_Ui.gd
+++ scene's/levels/Math/finish_game/Finish_Ui.gd
@@ -1,14 +1,24 @@
 extends Control
+var wrong
 
 
 
-
-onready var cat = $TextureRect/TextureRect2/character2
-onready var dog = $TextureRect/TextureRect2/character
-onready var score = $TextureRect/score
-
+onready var cat = $panel/ribbon/character
+onready var dog = $panel/ribbon/character2
+onready var score = $label
+export  var btnPath = "res://scene\'s/levels/Math/Board_calc.tscn"
 
 func _show(pose:int , text:String):
+	visible = true
 	cat.curent_pos = pose
 	dog.curent_pos = pose
-	score = text
+	score.text = text
+
+func _on_exit_pressed():
+	
+	visible = false
+#
+#
+#func _on_answers_result(result):
+#	wrong=result
+#	pass # Replace with function body.
