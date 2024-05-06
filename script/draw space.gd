7d8bc8eed6a7df8a8f9065f8df09d427983c7d78
Yanix <monnah.yans@gmail.com>
2024-05-06 12:26:26 +0100

    adding line size change

--- script/draw space.gd
+++ script/draw space.gd
@@ -12,7 +12,7 @@ var pressed = false
 var curent_line: Line2D
 var image:Image
 var old_child
-
+var changing_width = false
 signal send_img
 
 func _ready():
@@ -20,7 +20,7 @@ func _ready():
 	lines.width = width
 
 func _input(event: InputEvent) -> void:
-	if event is InputEventMouseButton:
+	if  event is InputEventMouseButton :
 		pressed = event.pressed
 		
 		# Making line
@@ -33,7 +33,7 @@ func _input(event: InputEvent) -> void:
 			emit_signal("send_img")
 			old_child = lines.get_point_count()
 	# Drawing lines
-	if event is InputEventMouseMotion && pressed:
+	if event is InputEventMouseMotion && pressed && changing_width == false :
 		curent_line.add_point(event.position - rect_position)
 
 
@@ -60,3 +60,14 @@ func _save(path:String = "res://captured_image.png"):
 
 func _on_send_color(modulate):
 	lines.default_color = modulate
+
+
+func _on_HSlider_drag_ended(value_changed):
+	changing_width = false
+	lines.width = $"../../../HSlider".value
+
+
+
+func _on_HSlider_drag_started():
+	changing_width = true
+	print("test")
